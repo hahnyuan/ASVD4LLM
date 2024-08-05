0f0d26c2b3c5eb30e1b86844cc221e6245713cf8
Zhihang Yuan <hahnyuan@gmail.com>
2024-08-05 05:16:32 +0000

    binary search kv compression bug fix

--- binary_search.py
+++ binary_search.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from evaluate_utils import evaluate_model, evaluate_perplexity
 from modules.svd_linear import SVDLinear, GradSVDLinear
 from tqdm import tqdm
+import time
 
 
 def binary_search_truncation_rank(model, sensitivity_dict, calib_loader, args):
@@ -25,43 +26,49 @@ def binary_search_truncation_rank(model, sensitivity_dict, calib_loader, args):
             else:
                 modules.append(raw_linear)
 
-    sensitivity_list = []
     if args.compress_kv_cache:
-        # after compression, we have 2 matrices, so kv_cache reduction is the twice of weight reduction
-        param_ratio_target = args.kv_cache_ratio_target * 2
+        ratio_target = args.kv_cache_ratio_target
         sensitivity_dict = {k: v for k, v in sensitivity_dict.items() if "k_proj" in k or "v_proj" in k}
         assert args.ppl_target < 0, "ppl_target is not supported when compressing kv_cache"
+        default_param_ratio = 2
     else:
-        param_ratio_target = args.param_ratio_target
+        ratio_target = args.param_ratio_target
+        default_param_ratio = 1
 
+    print(
+        f"=== {'compress kv_cache' if args.compress_kv_cache else 'compress weight'} target: ppl={args.ppl_target}, ratio_target={ratio_target} ==="
+    )
+
+    sensitivity_list = []
     for layername, v in sensitivity_dict.items():
-        for ratio, ppl in v.items():
-            if not args.compress_kv_cache and ratio >= 1:
+        for param_ratio, ppl in v.items():
+            if not args.compress_kv_cache and param_ratio >= 1:
                 # we need to compress the weights, so parameter ratio should be less than 1
                 continue
-            sensitivity_list.append((layername, ratio, ppl))
+            sensitivity_list.append((layername, param_ratio, ppl))
     sorted_sensitive_list = sorted(sensitivity_list, key=lambda x: -x[2])
 
     # binary search
     high = len(sorted_sensitive_list) - 1
     low = 0
-    assert args.ppl_target > 0 or param_ratio_target > 0
+    assert args.ppl_target > 0 or ratio_target > 0
 
     input_ids = torch.cat([_["input_ids"] for _ in calib_loader], 0)
     while low < high:
         mid = (low + high) // 2
-        layers_min_ratio = {layername: 1 for layername in sensitivity_dict.keys()}
-        for layername, ratio, ppl in sorted_sensitive_list[mid:]:
-            layers_min_ratio[layername] = min(layers_min_ratio[layername], ratio)
+        layers_min_ratio = {layername: default_param_ratio for layername in sensitivity_dict.keys()}
+        for layername, param_ratio, ppl in sorted_sensitive_list[mid:]:
+            layers_min_ratio[layername] = min(layers_min_ratio[layername], param_ratio)
         tot_params = 0
         compress_params = 0
         if args.ppl_target > 0:
-            for layername, ratio in layers_min_ratio.items():
+            assert not args.compress_kv_cache, "ppl_target is not supported when compressing kv_cache now"
+            for layername, param_ratio in layers_min_ratio.items():
                 raw_linear = module_dict[layername]
                 info = linear_info[raw_linear]
                 svd_linear = SVDLinear.from_linear(
                     raw_linear,
-                    param_ratio=ratio,
+                    param_ratio=param_ratio,
                     alpha=args.alpha,
                     act_aware=args.act_aware,
                     sigma_fuse=args.sigma_fuse,
@@ -69,7 +76,7 @@ def binary_search_truncation_rank(model, sensitivity_dict, calib_loader, args):
                 )
                 setattr(info["father"], info["name"], svd_linear)
                 tot_params += raw_linear.weight.numel()
-                compress_params += raw_linear.weight.numel() * ratio
+                compress_params += raw_linear.weight.numel() * param_ratio
             ppl = evaluate_perplexity(model, input_ids, args.n_calib_samples)
             param_ratio = compress_params / tot_params
             msg = f"low={low} mid={mid}, high={high}, ppl={ppl}, param_ratio={param_ratio}"
@@ -79,41 +86,47 @@ def binary_search_truncation_rank(model, sensitivity_dict, calib_loader, args):
             else:
                 low = mid + 1
         else:
-            for layername, ratio in layers_min_ratio.items():
+            for layername, param_ratio in layers_min_ratio.items():
                 raw_linear = module_dict[layername]
                 tot_params += raw_linear.weight.numel()
-                compress_params += raw_linear.weight.numel() * ratio
-            param_ratio = compress_params / tot_params
-            msg = f"low={low} mid={mid}, high={high}, param_ratio={param_ratio}({compress_params}/{tot_params})"
+                compress_params += raw_linear.weight.numel() * param_ratio
+            now_ratio = compress_params / tot_params
+            if args.compress_kv_cache:
+                # because param ratio is the params for ALinear+BLienar, so the rank ratio is param ratio/2
+                now_ratio /= 2
+            msg = f"low={low} mid={mid}, high={high}, now_ratio={now_ratio}, params=({compress_params}/{tot_params})"
             print(msg)
-            if param_ratio > param_ratio_target:
+            if now_ratio > ratio_target:
                 high = mid
             else:
                 low = mid + 1
 
-    print(f"Searching finished, decomposing layers...")
-    layers_min_ratio = {layername: None for layername in sensitivity_dict.keys()}
-    for layername, ratio, ppl in sorted_sensitive_list[mid:]:
+    print(f"=== Searching done, decomposing layers... ===")
+    layers_min_ratio = {layername: default_param_ratio for layername in sensitivity_dict.keys()}
+    for layername, param_ratio, ppl in sorted_sensitive_list[mid:]:
         if layers_min_ratio[layername] is None:
-            layers_min_ratio[layername] = ratio
+            layers_min_ratio[layername] = param_ratio
         else:
-            layers_min_ratio[layername] = min(layers_min_ratio[layername], ratio)
-    for layername, ratio in tqdm(layers_min_ratio.items()):
+            layers_min_ratio[layername] = min(layers_min_ratio[layername], param_ratio)
+    st = time.time()
+    for layername, param_ratio in tqdm(layers_min_ratio.items()):
         # set ratio
         raw_linear = module_dict[layername]
         info = linear_info[raw_linear]
-        if ratio is None:
+        if param_ratio is None:
             svd_linear = raw_linear
         else:
             svd_linear = SVDLinear.from_linear(
                 raw_linear,
-                param_ratio=ratio,
+                param_ratio=param_ratio,
                 alpha=args.alpha,
                 act_aware=args.act_aware,
                 sigma_fuse=args.sigma_fuse,
                 rank_align=args.rank_align,
             )
         setattr(info["father"], info["name"], svd_linear)
+    ed = time.time()
+    print(f"decompose time: {ed-st}")
 
 
 def binary_search_truncation_rank_optimize_scale(model, sensitivity_dict, calib_loader, args):
--- experiments/kv_cache_longbench.sh
+++ experiments/kv_cache_longbench.sh
@@ -1,16 +1,16 @@
 
-CUDA_VISIBLE_DEVICES='5' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.9 --compress_kv --eval_tasks small_longbench
-CUDA_VISIBLE_DEVICES='5' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.8 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='5' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.7 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='5' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.6 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='5' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.5 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='5' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.4 --use_cache --compress_kv --eval_tasks small_longbench 
+CUDA_VISIBLE_DEVICES='0' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.9 --use_cache --compress_kv --eval_tasks small_longbench &
+CUDA_VISIBLE_DEVICES='1' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.8 --use_cache --compress_kv --eval_tasks small_longbench &
+CUDA_VISIBLE_DEVICES='2' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.7 --use_cache --compress_kv --eval_tasks small_longbench &
+CUDA_VISIBLE_DEVICES='3' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.6 --use_cache --compress_kv --eval_tasks small_longbench &
+CUDA_VISIBLE_DEVICES='4' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.5 --use_cache --compress_kv --eval_tasks small_longbench &
+CUDA_VISIBLE_DEVICES='5' python asvd.py --model_id="models/Llama-2-7b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.4 --use_cache --compress_kv --eval_tasks small_longbench &
 
 
 
-CUDA_VISIBLE_DEVICES='6' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.9 --compress_kv --eval_tasks small_longbench
-CUDA_VISIBLE_DEVICES='6' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.8 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='6' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.7 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='6' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.6 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='6' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.5 --use_cache --compress_kv --eval_tasks small_longbench 
-CUDA_VISIBLE_DEVICES='6' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.4 --use_cache --compress_kv --eval_tasks small_longbench 
+CUDA_VISIBLE_DEVICES='7' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.9 --use_cache --compress_kv --eval_tasks small_longbench
+CUDA_VISIBLE_DEVICES='7' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.8 --use_cache --compress_kv --eval_tasks small_longbench 
+CUDA_VISIBLE_DEVICES='7' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.7 --use_cache --compress_kv --eval_tasks small_longbench 
+CUDA_VISIBLE_DEVICES='7' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.6 --use_cache --compress_kv --eval_tasks small_longbench 
+CUDA_VISIBLE_DEVICES='7' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.5 --use_cache --compress_kv --eval_tasks small_longbench 
+CUDA_VISIBLE_DEVICES='7' python asvd.py --model_id="models/Llama-2-13b-hf" --act_aware --alpha 0.5 --n_calib_samples 32 --scaling_method abs_mean --kv_cache_ratio_target 0.4 --use_cache --compress_kv --eval_tasks small_longbench 
